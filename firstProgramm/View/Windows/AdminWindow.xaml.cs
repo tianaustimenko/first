c6526ac8f7c1c6cc3edd4843533c2bff9209e079
eldar <etu@gmailcom>
2022-09-20 16:44:43 +0300

    initial

---
+++ firstProgramm/View/Windows/AdminWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace firstProgramm.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnLoadUsers_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new UsersPage());
+        }
+
+        private void btnLoadSomePage_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new SomePage());
+        }
+    }
+}
---
+++ firstProgramm/View/Windows/AutorizationWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace firstProgramm.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AutorizationWindow.xaml
+    /// </summary>
+    public partial class AutorizationWindow : Window
+    {
+        private List<User> _users;
+        public AutorizationWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserLogic.Validate(tbLogin.Text, tbPassword.Password))
+            {
+                new AdminWindow().Show();
+                Close();
+            }
+            else MessageBox.Show("Ошибка", "Неправильный логин или пароль",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+    }
+}
